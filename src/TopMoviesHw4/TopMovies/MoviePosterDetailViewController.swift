d839689b5bdba402887e11d1a359544f1353e6ed
Apoorva <apoorvamoodbidri14@gmail.com>
2016-01-20 17:02:28 -0800

    update to hw4

---
+++ src/TopMoviesHw4/TopMovies/MoviePosterDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MoviePosterDetailViewController.swift
+//  TopMovies
+//
+//  Created by Apoorva Moodbidri on 1/20/16.
+//  Copyright © 2016 GA Student. All rights reserved.
+//
+
+import UIKit
+
+class MoviePosterDetailViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var moviePosterScrollView: UIScrollView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
